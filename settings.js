1c5700f4e304b23a0f5956452d3967d4e24fe731
salwamd <115639798+salwamd@users.noreply.github.com>
2022-11-04 12:28:37 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "4KEYS 3S" //namabot kalian
-global.ownername= "4K SANZZ" //nama kalian
+global.botname = "j e g a BOT" //namabot kalian
+global.ownername= "AGA" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/4KSanzz" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
+global.email = "hakaren64@gmail.com" //bebas
+global.region = "paris" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["6281236167286","6281236167286"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "6281236167286" // nomor wa kalian
-global.ownernomerr = "+6281236167286" //nmr wa kalian
+global.premium = ['6282144607416'] //premium user
+global.owner = ["6282144607416","6285810326967"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282144607416" // nomor wa kalian
+global.ownernomerr = "+6282144607416" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© agaXjega' //sticker wm ubah
+global.author = 'Di Buat Oleh j e g a' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
